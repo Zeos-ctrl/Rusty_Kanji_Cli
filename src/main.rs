ba9b6f299cbcd837e61823b16b1ca384da08540f
Zeos <zeos@pop-os.localdomain>
2022-06-04 15:26:44 +0100

    Added sound file support and started on error handling

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,39 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::io::Result;
 use rand::*;
+use soloud::*;
 
+struct Kanjidata {
+    number: String,
+    level: String,
+    kanjichar: String,
+    hiragana: String,
+    meaning: String,
+    spoken_kanji: String,
+    word_type: String,
+    sentence_1: String,
+    sentence_2: String,
+    sentence_eng: String,
+    sentence_spoken: String,
+    kanji_hira: String,
+    sentence_3: String,
+    sentence_missing: String,
+}
 //Function to generate a random number to find a line
 fn randomnum() -> u16{return rand::thread_rng().gen_range(1..6000)}
 
-fn readfile() -> Result<()>{
+fn getkanji() -> Kanjidata{
     
     //Open file and buffered reader
-    let file = File::open("./KanjiFile.csv").expect("File not found");
+    let file = File::open("./KanjiFile.csv");
+
+    let file = match file{
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Problem opening file: {:?}", error)
+        },
+    };
+
     let mut reader = BufReader::new(file);
     let mut line = String::new();
    
@@ -24,25 +49,86 @@ fn readfile() -> Result<()>{
 
     //Get length of the vector
     let length = &kanji.len();
+    
+    let info = Kanjidata { 
+        number: kanji[length-14].to_string(),
+        level: kanji[length-13].to_string(),
+        kanjichar: kanji[length-12].to_string(),
+        hiragana: kanji[length-11].to_string(),
+        meaning: kanji[length-10].to_string(),
+        spoken_kanji: kanji[length-9].to_string(),
+        word_type: kanji[length-8].to_string(),
+        sentence_1: kanji[length-7].to_string(),
+        sentence_2: kanji[length-6].to_string(),
+        sentence_eng: kanji[length-5].to_string(),
+        sentence_spoken: kanji[length-4].to_string(),
+        kanji_hira: kanji[length-3].to_string(),
+        sentence_3: kanji[length-2].to_string(),
+        sentence_missing: kanji[length-1].to_string(),
+    };
 
-    output(&kanji,&length).expect("Error outputting Data");
-
-    Ok(())
+    return info;
     
 }
 
 //Output function to the terminal
-fn output(kanji: &Vec<&str>,length: &usize) -> Result<()>{
-    println!("Level: {:?}", &kanji[length - 13]);
-    println!("Kanji: {:?}",&kanji[length -12]);
-    println!("Hiragana: {:?}", &kanji[length -11]);
-    println!("Meaning: {:?}", &kanji[length-10]);
-    println!("Sentence Example: {:?}", &kanji[length - 2]);
-    println!("Sentence English: {:?}", &kanji[length - 5]);
-    println!("Sentence Missing: {:?}", &kanji[length - 1].trim_end());
+fn output(kanji: &Kanjidata) -> Result<()>{
+    println!("List Number: {:?}", &kanji.number);
+    println!("Level: {:?}",&kanji.level);
+    println!("Kanji: {:?}",&kanji.kanjichar);
+    println!("Mixed Character: {:?}", &kanji.kanji_hira);
+    println!("Hiragana: {:?}", &kanji.hiragana);
+    println!("Meaning: {:?}", &kanji.meaning);
+    println!("Word type: {:?}", &kanji.word_type);
+    println!("Sentence Example: {:?}", &kanji.sentence_3);
+    println!("Sentence English: {:?}", &kanji.sentence_eng);
+    println!("Sentence Missing: {:?}", &kanji.sentence_missing);
+    Ok(())
+}
+
+fn playsound(sound: &str) -> Result<()>{
+
+    //Removes the begining part of the file path being [ sound:....]
+    let fronttrim: Vec<&str> = sound.split(":").collect();
+    //Formats the filepath from the vector
+    let filepath  = format!("{}{}","./soundpack/",fronttrim[1]
+                            .to_string()
+                            .trim_end_matches(']'));
+
+   //Using the crate Soloud this opens and plays the vocal sound using the
+   //example displayed on the crate page
+    let soloud = Soloud::default().expect("Failed to start Soloud");
+    let mut audio = audio::Wav::default();
+    audio.load(&std::path::Path::new(&filepath)).expect("File not loaded");
+
+    soloud.play(&audio);
+    while soloud.voice_count() > 0 {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+    
+
     Ok(())
 }
 
 fn main() {
-    readfile().expect("Didnt work");
+    //Add Menu
+    //Add Error Handling
+    let kanji = getkanji();
+
+    let output = output(&kanji);
+    let _output = match output{
+        Ok(output) => output,
+        Err(error) => {
+            panic!("Output Function Failed: {:?}", error)
+        },
+    };
+
+    let sound = playsound(&kanji.spoken_kanji);
+    let _sound = match sound{
+        Ok(sound) => sound,
+        Err(error) => {
+            panic!("Failed To play sound: {:?}", error)
+        },
+    };
+
 }
